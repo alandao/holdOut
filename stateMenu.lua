d2a482885ff9afe3ad78948f20e4719fa38ad5c7
Isenberg01 <michaelice95@hotmail.com>
2015-06-03 11:12:25 -0700

    updated options button

--- stateMenu.lua
+++ stateMenu.lua
@@ -9,21 +9,20 @@ function menu:CreateToolbar()
 
 	local width = love.graphics.getWidth()
 	local version = 0.1
-	
 	local toolbar = Loveframes.Create("panel")
 	toolbar:SetSize(width, 35)
 	toolbar:SetPos(0, 0)
 	toolbar:SetState("mainmenu")
-	
+
 	local info = Loveframes.Create("text", toolbar)
 	info:SetPos(5, 3)
 	info:SetState("mainmenu")
 	info:SetText({
-		{color = {0, 0, 0, 255}}, 
+		{color = {0, 0, 0, 255}},
 		"HoldOut",
-		{color = {129, 87, 255, 255}}, " version " ..version, 
+		{color = {129, 87, 255, 255}}, " version " ..version,
 	})
-	
+
 	menu.buttonConnect = Loveframes.Create("button", toolbar)
 	menu.buttonConnect:SetPos(toolbar:GetWidth() - 105, 5)
 	menu.buttonConnect:SetSize(100, 25)
@@ -32,7 +31,7 @@ function menu:CreateToolbar()
 	menu.buttonConnect.OnClick = function()
 		menu:CreateConnectFrame()
 	end
-	
+
 	menu.buttonHost = Loveframes.Create("button", toolbar)
 	menu.buttonHost:SetPos(toolbar:GetWidth() - 210, 5)
 	menu.buttonHost:SetSize(100, 25)
@@ -41,6 +40,16 @@ function menu:CreateToolbar()
 	menu.buttonHost.OnClick = function()
 		menu:CreateHostFrame()
 	end
+
+	menu.buttonOptions = Loveframes.Create("button", toolbar)
+	menu.buttonOptions:SetPos(toolbar:GetWidth()-315, 5)
+	menu.buttonOptions:SetSize(100,25)
+	menu.buttonOptions:SetState("mainmenu")
+	menu.buttonOptions:SetText("Options")
+	menu.buttonOptions.OnClick = function() menu:CreateOptionsFrame()
+
+	end
+
 end
 
 function menu:CreateConnectFrame()
@@ -51,29 +60,29 @@ function menu:CreateConnectFrame()
 	connectFrame:SetModal(true)
 	connectFrame:Center()
 	connectFrame:SetState("mainmenu")
-	
+
 	local textIP = Loveframes.Create("text", connectFrame)
 	textIP:SetText("IP: ")
 	textIP:SetPos(10, 36)
-	
+
 	local textInputIP = Loveframes.Create("textinput", connectFrame)
 	textInputIP:SetPos(100, 30)
 	textInputIP:SetWidth(190)
-	
+
 	local textPort = Loveframes.Create("text", connectFrame)
 	textPort:SetText("Port: ")
 	textPort:SetPos(10, 80)
-	
+
 	local textInputPort = Loveframes.Create("textinput", connectFrame)
 	textInputPort:SetPos(220, 74)
 	textInputPort:SetWidth(70)
-	
+
 	--this button on click will host a server.
 	local buttonConnect = Loveframes.Create("button", connectFrame)
 	buttonConnect:SetPos(10, 110)
 	buttonConnect:SetSize(280, 80)
 	buttonConnect:SetText("Connect")
-	
+
 end
 
 function menu:CreateHostFrame()
@@ -84,19 +93,47 @@ function menu:CreateHostFrame()
 	hostFrame:SetModal(true)
 	hostFrame:Center()
 	hostFrame:SetState("mainmenu")
-	
+
 	local textInput = Loveframes.Create("textinput", hostFrame)
 	textInput:SetPos(220, 34)
 	textInput:SetWidth(70)
-	
+
 	local textPort = Loveframes.Create("text", hostFrame)
 	textPort:SetText("Port: ")
 	textPort:SetPos(10, 40)
-	
+
 	--this button on click will host a server.
 	local buttonCreateServer = Loveframes.Create("button", hostFrame)
 	buttonCreateServer:SetPos(10, 70)
 	buttonCreateServer:SetSize(280, 60)
 	buttonCreateServer:SetText("Create Server")
-	
+
+end
+
+function menu:CreateOptionsFrame()
+	local optionsFrame = Loveframes.Create("frame")
+	optionsFrame:SetName("Options")
+	optionsFrame:SetSize(220,140)
+	optionsFrame:SetDraggable(false)
+	optionsFrame:SetModal(true)
+	optionsFrame:Center()
+	optionsFrame:SetState("mainmenu")
+
+	local buttonControls = Loveframes.Create("button", optionsFrame)
+	buttonControls:SetPos(20,70)
+	buttonControls:SetSize(80,50)
+	buttonControls:SetText("Controls")
+	buttonControls.OnClick = function() print("testing control clicks")
+
+	end
+
+	local buttonHelp = Loveframes.Create("button", optionsFrame)
+	buttonHelp:SetPos(120,70)
+	buttonHelp:SetSize(80,50)
+	buttonHelp:SetText("Help")
+
+	buttonHelp.OnClick = function() print("testing help clicks")
+
+	end
+
 end
