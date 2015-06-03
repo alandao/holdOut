890ea39bfd8e4ad05ca0ff52145cf07f1721b47a
Alan on Lenovo <alan.n.dao@gmail.com>
2015-06-03 12:48:51 -0700

    initial work with enet

--- client/init.lua
+++ client/init.lua
@@ -5,27 +5,30 @@ client = {}
 function client:enter(...)
 	print("Entered client state")
 	Loveframes.SetState("ingame")
-	
+
 	--2 is the second argument in Gamestate.switch(...)
 	local clientconfig = ({...})[2]
-	
+
 	host = enet.host_create()
 	server = host:connect(clientconfig.ip .. ":" .. clientconfig.port)
-	
+	done = false
+
 end
 
 function client:update(dt)
-	local event = host:service(0)
-	while event do
-		if event.type == "receive" then
-			print("Got message: ", event.data, event.peer)
-			event.peer:send( "ping" )
-		elseif event.type == "connect" then
-			print(event.peer " connected.")
-			event.peer:send( "ping" )
-		elseif event.type == "disconnect" then
-			print(event.peer .. " disconnected.")
-		end
-		event = host:service()
+	local event = host:service(100)
+	if event then
+		if event.type == "connect" then
+			print("Connected to", event.peer)
+			event.peer:send("hello world")
+    elseif event.type == "receive" then
+      print("Got message: ", event.data, event.peer)
+      done = true
+    end
 	end
+  if done then
+    server:disconnect()
+    host:flush()
+    Gamestate.switch(menu)
+  end
 end
--- listenserver/init.lua
+++ listenserver/init.lua
@@ -6,4 +6,9 @@ function listenserver:enter(...)
 	--indexes do not start at 0 in this language
 	local serverconfig = ({...})[2]
 	
+	server = love.thread.newThread("server/serverthread.lua")
+	c1 = love.thread.getChannel("debugprint")
+	server:start(c1)
+	
+	print("started thread")
 end
