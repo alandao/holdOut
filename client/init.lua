f39b409d189ff6e2376e75624bb227cf628c121d
Alan on Lenovo <alan.n.dao@gmail.com>
2015-06-04 16:17:09 -0700

    initial work done with connecting and disconnecting

--- client/init.lua
+++ client/init.lua
@@ -3,7 +3,7 @@ require "enet"
 client = {}
 
 function client:enter(...)
-	print("Entered client state")
+	clientPrint("Entered client state")
 	Loveframes.SetState("ingame")
 
   channel = love.thread.getChannel("debug") --will contain a queue of debug messages
@@ -12,19 +12,26 @@ function client:enter(...)
 	local clientconfig = ({...})[2]
 	host = enet.host_create()
 	server = host:connect(clientconfig.ip .. ":" .. clientconfig.port)
-
 end
 
 function client:update(dt)
+  --get messages from server
+  if channel:peek() ~= nil then
+    print(channel:pop())
+  end
+
 	local event = host:service(0.03)
-  print(channel:pop())
 	if event then
 		if event.type == "connect" then
-      print("Connected to", event.peer)
+      clientPrint("Connected to " .. host:get_socket_address())
 			event.peer:send("hello world")
     elseif event.type == "receive" then
-      print("Got message: ", event.data, event.peer)
+      print("Client: Got message: ", event.data, event.peer)
       event.peer:send("hello world always")
     end
 	end
+end
+
+function clientPrint(message)
+  print("Client: " .. message)
 end
--- server/main_server.lua
+++ server/main_server.lua
@@ -2,21 +2,34 @@ require "enet"
 
 --server is meant to be run as a thread, doesn't need any Love2d.
 
+
 local port = ...
 
+
+
 c = love.thread.getChannel("debug")
-c:push("Starting server on port " .. port)
+
+function debugPush(x)
+  c:push("Server: " .. x)
+end
+
+debugPush("Starting server on port " .. port)
+
 
 local host = enet.host_create("localhost:" .. port)
+
+--the worldstate is the state shared by all people
+local world = {}
 
 while true do
 	local event = host:service(0.03)
 	if event then
     if event.type == "connect" then
+      debugPush(event.peer:connect_id() .. " has connected.")
     end
     if event.type == "receive" then
         --c.push("Got message: " .. event.data .. event.peer)
-        event.peer:send(event.data)
+        --event.peer:send(event.data)
     end
     if event.type == "disconnect" then
 
@@ -25,3 +38,5 @@ while true do
 
   --do gameloop after this.
 end
+
+
